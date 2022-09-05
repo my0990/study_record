c78fbe573a3ccb5d2cdc04caaa2bd01946205795
my0990 <my0990@naver.com>
2022-09-05 16:39:38 +0900

    220905 #1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {Route, Routes} from 'react-router-dom';
+import AuthPage from './pages/AuthPage';
+import PostPage from './pages/PostPage';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<AuthPage/>}/>
+      <Route path="/post" element={<PostPage/>}/>
+    </Routes>
   );
 }
 
---
+++ src/pages/PostPage.js
@@ -0,0 +1,9 @@
+const PostPage = () => {
+    return(
+        <div>
+            post페이지입니다
+        </div>
+    )
+}
+
+export default PostPage;
