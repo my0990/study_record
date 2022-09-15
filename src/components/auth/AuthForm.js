7b6c31d45c87958296038f36091d1d91122dbd95
my0990 <my0990@naver.com>
2022-09-15 20:37:38 +0900

    220915 mistake

--- src/components/auth/AuthForm.js
+++ src/components/auth/AuthForm.js
@@ -11,7 +11,7 @@ const AuthForm = ({onLogin, onChange, form}) =>{
                     <h1 className="text-3xl font-semibold text-center text-purple-700">공부인증</h1>
                     <form className="mt-6" onSubmit={onLogin}>
                         <div>
-                            <label htmlFor="email" className="block text-sm text-gray-800">Email</label>
+                            <label htmlFor="email" className="block text-sm text-gray-800" autocomplete="off">name</label>
                             <input  className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40" onChange={onChange} name="username" value={form.username}/>
                             
                         </div>
